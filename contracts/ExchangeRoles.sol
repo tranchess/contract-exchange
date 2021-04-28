f717391ca5925984764672a1e13be4a5925681d7
Terry Pawn <terry.pawn@tranchess.com>
2021-04-28 09:10:51 +0000

    Allow zero maker requirement

--- contracts/ExchangeRoles.sol
+++ contracts/ExchangeRoles.sol
@@ -17,8 +17,8 @@ abstract contract ExchangeRoles {
     /// @notice Minimum vote-locked governance token balance required to place maker orders.
     uint256 public immutable makerRequirement;
 
-    /// @notice Mapping of account => maker expiration timestamp
-    mapping(address => uint256) public makerExpiration;
+    /// @dev Mapping of account => maker expiration timestamp
+    mapping(address => uint256) internal _makerExpiration;
 
     constructor(address votingEscrow_, uint256 makerRequirement_) public {
         votingEscrow = IVotingEscrow(votingEscrow_);
@@ -32,19 +32,27 @@ abstract contract ExchangeRoles {
         _;
     }
 
+    /// @notice Returns maker expiration timestamp of an account.
+    ///         When `makerRequirement` is zero, this function always returns
+    ///         an extremely large timestamp (2500-01-01 00:00:00 UTC).
+    function makerExpiration(address account) external view returns (uint256) {
+        return makerRequirement > 0 ? _makerExpiration[account] : 16725225600;
+    }
+
     /// @notice Verify if the account is an active maker or not
     /// @param account Account address to verify
     /// @return True if the account is an active maker; else returns false
     function isMaker(address account) public view returns (bool) {
-        return makerExpiration[account] > block.timestamp;
+        return makerRequirement == 0 || _makerExpiration[account] > block.timestamp;
     }
 
     /// @notice Apply for maker membership
     function applyForMaker() external {
+        require(makerRequirement > 0, "No need to apply for maker");
         // The membership will be valid until the current vote-locked governance
         // token balance drop below the requirement.
         uint256 expiration = votingEscrow.getTimestampDropBelow(msg.sender, makerRequirement);
-        makerExpiration[msg.sender] = expiration;
+        _makerExpiration[msg.sender] = expiration;
         emit MakerApplied(msg.sender, expiration);
     }
 }
